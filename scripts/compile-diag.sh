a266ded2e3b0e84e6706e2d648b790faf1d2c6e4
Eric Stern <stormalong@gmail.com>
2025-12-09 15:37:36 -0800

    updated script

--- scripts/compile-diag.sh
+++ scripts/compile-diag.sh
@@ -7,17 +7,18 @@ if [ -z "$1" ]; then
 fi
 
 # remove previous outputs
-rm -f temp/unoptimized.ir temp/unoptimized.exe temp/optimized.ir temp/optimized.exe
+rm -f temp/unoptimized.ir temp/unoptimized.asm temp/unoptimized.exe temp/optimized.ir temp/optimized.asm temp/optimized.exe
 
 SOURCE="$1"
 
 # Compile unoptimized with IR
-./bin/maxon compile "$SOURCE" --emit-ir -vvv -o "temp/unoptimized.exe" > temp/unoptimized.compiler.log 2>&1
+./bin/maxon compile "$SOURCE" --emit-ir --emit-asm -vvv -o "temp/unoptimized.exe" > temp/unoptimized.compiler.log 2>&1
 if [ $? -ne 0 ]; then
 	echo "unopt Compilation failed"
 	exit 1
 fi
 echo "wrote temp/unoptimized.ir"
+echo "wrote temp/unoptimized.asm"
 echo "wrote temp/unoptimized.exe"
 echo "wrote temp/unoptimized.compiler.log"
 
@@ -28,12 +29,13 @@ echo "unoptimized exit code: $?"
 
 echo ""
 # Compile optimized with IR
-./bin/maxon compile "$SOURCE" --emit-ir -O -vvv -o "temp/optimized.exe" > temp/optimized.compiler.log 2>&1
+./bin/maxon compile "$SOURCE" --emit-ir --emit-asm -O -vvv -o "temp/optimized.exe" > temp/optimized.compiler.log 2>&1
 if [ $? -ne 0 ]; then
 	echo "optimized Compilation failed"
 	exit 1
 fi
 echo "wrote temp/optimized.ir"
+echo "wrote temp/optimized.asm"
 echo "wrote temp/optimized.exe"
 echo "wrote temp/optimized.compiler.log"
 
